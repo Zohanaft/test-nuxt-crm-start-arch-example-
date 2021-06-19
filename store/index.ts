fc33350eddcef298084e0a7a0b2ceb965699e3d5
Zohanaft <zohaosipov@gmail.com>
2021-06-20 01:31:12 +0300

    fix store config options

--- store/index.ts
+++ store/index.ts
@@ -1,9 +1,11 @@
-// import { store } from 'quasar/wrappers';
-import Vuex from 'vuex'
+import Vuex, { StoreOptions } from 'vuex'
 import Vue from 'vue'
+
+// object -> store options interface
+type storeOptions = object | null | undefined
 
 Vue.use(Vuex)
 
-export const store = new Vuex.Store<any>({
+export const store = new Vuex.Store<StoreOptions<storeOptions>>({
   strict: !!process.env.DEV
 })
